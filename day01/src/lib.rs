cb74cfe34e15676a8d6322d2257d03111b4fbb3a
Franco Nieddu <nieddu@taceo.io>
2024-12-03 22:27:35 +0100

    feat: fixed double parse

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,70 +1,54 @@
 use aoc_traits::AdventOfCodeDay;
 
-type Part1Input = (Vec<usize>, Vec<usize>);
-type Part2Input = (Vec<usize>, Vec<usize>);
+type Input = (Vec<usize>, Vec<usize>, Vec<usize>);
 #[derive(Default)]
 pub struct Solver;
 impl AdventOfCodeDay for Solver {
-    type ParsedInput<'a> = (Part1Input, Part2Input);
+    type ParsedInput<'a> = Input;
     type Part1Output = i64;
     type Part2Output = usize;
 
     fn solve_part1(input: &Self::ParsedInput<'_>) -> Self::Part1Output {
-        let (input, _) = input;
         solve_puzzle1(input)
     }
 
     fn solve_part2(input: &Self::ParsedInput<'_>) -> Self::Part2Output {
-        let (_, input) = input;
         solve_puzzle2(input)
     }
 
     fn parse_input<'a>(input: &'a str) -> Self::ParsedInput<'a> {
-        let input_1 = parse_input1(input);
-        let input_2 = parse_input2(input);
-        (input_1, input_2)
+        parse_input(input)
     }
 }
 
-fn parse_input1(str: &str) -> (Vec<usize>, Vec<usize>) {
+fn parse_input(str: &str) -> Input {
+    let mut count = vec![0; 100000];
     let (mut lhs, mut rhs) = str
         .lines()
         .map(|line| {
             let mut chars = line.split_whitespace();
             let left = chars.next().unwrap().parse::<usize>().unwrap();
             let right = chars.next_back().unwrap().parse::<usize>().unwrap();
+            count[right] += 1;
             (left, right)
         })
         .collect::<(Vec<_>, Vec<_>)>();
     lhs.sort();
     rhs.sort();
-    (lhs, rhs)
+    (lhs, rhs, count)
 }
 
-fn parse_input2(str: &str) -> (Vec<usize>, Vec<usize>) {
-    let mut vec = Vec::with_capacity(1000);
-    let mut count = vec![0; 100000];
-    for line in str.lines() {
-        let mut chars = line.split_whitespace();
-        let left = chars.next().unwrap().parse::<usize>().unwrap();
-        let right = chars.next_back().unwrap().parse::<usize>().unwrap();
-        count[right] += 1;
-        vec.push(left);
-    }
-    (vec, count)
-}
-
-fn solve_puzzle1(input: &Part1Input) -> i64 {
-    let (lhs, rhs) = input;
+fn solve_puzzle1(input: &Input) -> i64 {
+    let (lhs, rhs, _) = input;
     lhs.into_iter()
         .zip(rhs)
         .map(|(lhs, rhs)| (*lhs as i64 - *rhs as i64).abs())
         .sum()
 }
 
-fn solve_puzzle2(input: &Part2Input) -> usize {
-    let (lhs, rhs) = input;
-    lhs.into_iter().map(|x| x * rhs[*x]).sum()
+fn solve_puzzle2(input: &Input) -> usize {
+    let (lhs, _, count) = input;
+    lhs.into_iter().map(|x| x * count[*x]).sum()
 }
 
 #[test]
