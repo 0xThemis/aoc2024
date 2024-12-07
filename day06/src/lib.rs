05487e4a384d05d7aa1d02815679c81baea9f1a9
Franco Nieddu <nieddu@taceo.io>
2024-12-07 02:00:05 +0100

    feat: day06 part2

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -19,12 +19,12 @@ impl std::fmt::Debug for Field {
     }
 }
 
-#[derive(Clone, Debug, Copy)]
+#[derive(Clone, Debug, Copy, PartialEq, Eq)]
 enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
+    Up = 0,
+    Right = 1,
+    Down = 2,
+    Left = 3,
 }
 
 impl Default for Direction {
@@ -49,56 +49,93 @@ impl Guard {
         self.x = x;
         self.y = y;
     }
-    pub fn turn(&mut self) {
-        let direction = match self.direction {
-            Direction::Up => Direction::Right,
-            Direction::Right => Direction::Down,
-            Direction::Down => Direction::Left,
-            Direction::Left => Direction::Up,
-        };
-        self.direction = direction;
-    }
-
-    pub fn what_next_step(&mut self, map: &[Vec<Field>]) -> Field {
-        match self.direction {
-            Direction::Up => map[self.x - 1][self.y],
-            Direction::Right => map[self.x][self.y + 1],
-            Direction::Down => map[self.x + 1][self.y],
-            Direction::Left => map[self.x][self.y - 1],
-        }
-    }
-    pub fn go(&mut self) {
-        match self.direction {
-            Direction::Up => self.x -= 1,
-            Direction::Right => self.y += 1,
-            Direction::Down => self.x += 1,
-            Direction::Left => self.y -= 1,
-        }
-    }
 }
 
 pub struct Map {
     map: Vec<Vec<Field>>,
-    guard: Guard,
+    direction: Direction,
+    path: Vec<(usize, usize)>,
+    x: usize,
+    y: usize,
 }
 
 impl Map {
+    fn solve_part2(&self) -> usize {
+        let mut map = self.map.clone();
+        let mut first_path = self.path.clone().into_iter();
+        // not allowed on first
+        first_path.next().unwrap();
+        first_path.next_back().unwrap();
+
+        let mut counter = 0;
+        let mut already_taken = vec![vec![false; map[0].len()]; map.len()];
+
+        for (x, y) in first_path {
+            if already_taken[x][y] {
+                continue;
+            }
+            already_taken[x][y] = true;
+            map[x][y] = Field::Obstacle;
+            if self.is_loop(&map) {
+                counter += 1;
+            }
+            map[x][y] = Field::Free;
+        }
+        counter
+    }
+
+    fn is_loop(&self, map: &[Vec<Field>]) -> bool {
+        let mut visited = vec![vec![vec![false; 4]; map[0].len() + 1]; map.len() + 1];
+        let mut x = self.x;
+        let mut y = self.y;
+        let mut direction = self.direction;
+        let is_loop = loop {
+            // check if we are on circle
+            if visited[x][y][direction as usize] {
+                return true;
+            }
+            visited[x][y][direction as usize] = true;
+
+            match direction {
+                Direction::Up => match map[x - 1][y] {
+                    Field::Obstacle => {
+                        direction = Direction::Right;
+                    }
+                    Field::Free => x -= 1,
+                    Field::Over => break false,
+                },
+                Direction::Right => match map[x][y + 1] {
+                    Field::Obstacle => {
+                        direction = Direction::Down;
+                    }
+                    Field::Free => y += 1,
+                    Field::Over => break false,
+                },
+                Direction::Down => match map[x + 1][y] {
+                    Field::Obstacle => {
+                        direction = Direction::Left;
+                    }
+                    Field::Free => x += 1,
+                    Field::Over => break false,
+                },
+                Direction::Left => match map[x][y - 1] {
+                    Field::Obstacle => {
+                        direction = Direction::Up;
+                    }
+                    Field::Free => y -= 1,
+                    Field::Over => break false,
+                },
+            }
+        };
+        is_loop
+    }
+
     fn solve_part1(&self) -> usize {
         let mut visited = vec![vec![false; self.map[0].len()]; self.map.len()];
-        visited[self.guard.x][self.guard.y] = true;
-        let mut moving_guard = self.guard.clone();
-        loop {
-            match moving_guard.what_next_step(&self.map) {
-                Field::Obstacle => {
-                    visited[moving_guard.x][moving_guard.y] = true;
-                    moving_guard.turn();
-                }
-                Field::Free => visited[moving_guard.x][moving_guard.y] = true,
-                Field::Over => break,
-            }
-            moving_guard.go();
+        for (x, y) in self.path.iter() {
+            visited[*x][*y] = true;
         }
-        visited.into_iter().flatten().filter(|x| *x).count() + 1
+        visited.into_iter().flatten().filter(|x| *x).count()
     }
 }
 
@@ -114,7 +151,7 @@ impl AdventOfCodeDay for Solver {
     }
 
     fn solve_part2(input: &Self::ParsedInput<'_>) -> Self::Part2Output {
-        todo!()
+        input.solve_part2()
     }
 
     fn parse_input<'a>(input: &'a str) -> Self::ParsedInput<'a> {
@@ -148,11 +185,60 @@ fn parse_input(input: &str) -> Map {
     let width = first_line.len();
     map.push(vec![Field::Over; width]);
     map.push(parse_line(first_line, &mut guard, 0));
-    for (row, line) in input.lines().enumerate() {
-        map.push(parse_line(line, &mut guard, row));
+    for (row, line) in lines.enumerate() {
+        map.push(parse_line(line, &mut guard, row + 1));
     }
     map.push(vec![Field::Over; width]);
-    Map { guard, map }
+    Map {
+        path: get_path(guard.x, guard.y, guard.direction, &map),
+        map,
+        direction: guard.direction,
+        x: guard.x,
+        y: guard.y,
+    }
+}
+
+fn get_path(
+    mut x: usize,
+    mut y: usize,
+    mut direction: Direction,
+    map: &[Vec<Field>],
+) -> Vec<(usize, usize)> {
+    let mut path = Vec::with_capacity(map[0].len() * map.len());
+    loop {
+        path.push((x, y));
+        match direction {
+            Direction::Up => match map[x - 1][y] {
+                Field::Obstacle => {
+                    direction = Direction::Right;
+                }
+                Field::Free => x -= 1,
+                Field::Over => break,
+            },
+            Direction::Right => match map[x][y + 1] {
+                Field::Obstacle => {
+                    direction = Direction::Down;
+                }
+                Field::Free => y += 1,
+                Field::Over => break,
+            },
+            Direction::Down => match map[x + 1][y] {
+                Field::Obstacle => {
+                    direction = Direction::Left;
+                }
+                Field::Free => x += 1,
+                Field::Over => break,
+            },
+            Direction::Left => match map[x][y - 1] {
+                Field::Obstacle => {
+                    direction = Direction::Up;
+                }
+                Field::Free => y -= 1,
+                Field::Over => break,
+            },
+        }
+    }
+    path
 }
 
 #[test]
@@ -162,5 +248,5 @@ fn day06() {
     let parsed = Solver::parse_input(input.trim());
 
     assert_eq!(4973, Solver::solve_part1(&parsed));
-    //assert_eq!(6142, Solver::solve_part2(&parsed));
+    assert_eq!(1482, Solver::solve_part2(&parsed));
 }
